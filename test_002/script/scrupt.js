334560c5db04bd6680f7031019eb62218f92658f
chosigtoggi <127915600+xhRl2988@users.noreply.github.com>
2024-05-12 18:38:15 +0900

    Add files via upload

---
+++ test_002/script/scrupt.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function(){
+    $(".main>li").mouseover(function(){
+        $(this).find(".sub").stop().slideDown()
+    })
+    $(".main>li").mouseout(function(){
+        $(this).find(".sub").stop().slideUp()
+    })
+
+
+    $(".imgslide li:gt(0)").hide()
+    setInterval(function(){
+        $(".imgslide li:first").fadeOut().
+        next("li").fadeIn().
+        end().appendTo(".imgslide>ul")
+    },1000)
+})
